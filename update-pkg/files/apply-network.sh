6d5f97e99dff9c3d05e4dcd2b8316e5ee281dd65
hsoulsboy <hsoulsboy@gmail.com>
2023-04-22 04:17:11 -0300

    Change network configuration method

--- update-pkg/files/apply-network.sh
+++ update-pkg/files/apply-network.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-while true; do
-    sleep 120
-    netplan apply
-done
+IP=$1
+MASK=$2
+INTERFACE=$3
+
+if [ ! "$#" -eq 3 ]; then
+	echo "Usage: $0 interface ip mask"
+	exit 1
+fi
+
+INTERFACE=${1}
+IP=${2}
+MASK=${3}
+
+echo "Configuring ${INTERFACE} with IP address ${IP}/${MASK}"
+sudo ip a add ${IP}/${MASK} dev ${INTERFACE}
